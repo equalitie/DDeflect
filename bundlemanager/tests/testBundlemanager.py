121d79066b6e9d0897edc3630a88624c555540e8
Bill <bill.doran@gmail.com>
2014-09-05 13:14:35 -0400

    added manager test stub

---
+++ bundlemanager/tests/testBundlemanager.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python
+"""
+Python Modules
+"""
+import unittest
+import requests
+import os
+
+"""
+Bundle Manager modules
+"""
+from bundlemanager.lib.bundler import BundleMaker 
+from bundlemanager.bundlemanager import *
+
+"""
+Third party modules
+"""
+import requests
+
+class TestBundlemanager(unittest.TestCase):
+
+    def setUp(self):
+       pass 
