91624faf0ed65495f965e409f3cf1228e1d79165
Hugh Nowlan <nosmo@netsoc.tcd.ie>
2014-12-10 16:47:21 -0500

    bundlemanager: Add bundlemanager import to tornado

--- bundlemanager/src/tornado_starter.py
+++ bundlemanager/src/tornado_starter.py
@@ -4,6 +4,7 @@ from tornado.wsgi import WSGIContainer
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from bundler import settings
+from bundlemanager import DebundlerServer
 
 http_server = HTTPServer(WSGIContainer(DebundlerServer()))
 http_server.listen(settings.general["wsgi_port"])
